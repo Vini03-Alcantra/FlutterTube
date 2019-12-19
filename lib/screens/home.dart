1149aae5807b224ff9d9b5171a9835567606b6e3
Vinícius de Alcantra Barros <viniciusalcantarabarros@gmail.com>
2019-12-19 16:26:49 -0300

    estou feliz, pesquisa ilimitada

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,6 +7,7 @@ import 'package:fluttertube/widgets/videotile.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<VideosBloc>(context);
     return Scaffold(
       appBar: AppBar(
         title: Container(
@@ -30,21 +31,34 @@ class Home extends StatelessWidget {
             icon: Icon(Icons.search),
             onPressed: () async {
               String result = await showSearch(context: context, delegate: DataSearch());
-              if(result != null) BlocProvider.of<VideosBloc>(context).inSearch.add(result);
+              if(result != null) bloc.inSearch.add(result);
             },
           )
         ],
       ),
       backgroundColor: Colors.black87,
       body: StreamBuilder(
-        stream: BlocProvider.of<VideosBloc>(context).outVideos,
+        stream: bloc.outVideos,
+        initialData: [],
         builder: (context, snapshot){
           if(snapshot.hasData)
             return ListView.builder(
               itemBuilder: (context, index){
-                return VideoTile(snapshot.data[index]);
+                if(index < snapshot.data.length){
+                  return VideoTile(snapshot.data[index]);
+                }else if(index > 1){
+                  bloc.inSearch.add(null);
+                  return Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.red),),
+                  );
+                } else {
+                  return Container();
+                }                
               },
-              itemCount: snapshot.data.length,
+              itemCount: snapshot.data.length + 1,
             );
             else
               return Container();
